a1a8689fbc4ed01ba3841723ae1ffbc5c403b184
Heisenberg <medhajjari7@gmail.com>
2023-08-23 17:11:00 +0100

    Airflow Setup + Scraping spiders + Cleaning Script

---
+++ Airflow/dags/pipeline.py
@@ -0,0 +1,32 @@
+import os
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+
+workflow = DAG(
+    "Pipeline",
+    schedule_interval="0 6 1,15 * *",
+    start_date=datetime(2023, 7, 1)
+)
+
+with workflow:
+    scrap = BashOperator(
+        task_id='scrap_bourse',
+        bash_command='pwd'
+    )
+
+    clean = BashOperator(
+		task_id='clean_data',
+		bash_command= 'ls'
+    )
+
+    ingest = BashOperator(
+		task_id='ingest_data_database',
+		bash_command= 'ls'
+    )
+
+    scrap >> clean >> ingest
+
